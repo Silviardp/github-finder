da37258d35db8cafc5ed39ec7b9c2cf3b3fad81e
Silvia Rocha <silviadprocha@gmail.com>
2022-01-18 17:10:05 +0100

    updated token

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -23,14 +23,14 @@ function UserResults() {
 
  if(!loading) {
  return (
-  <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:gris-cols-2'>
+  <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
    {users.map((user) => (
     <h3>{user.login}</h3>
    ))}
   </div>
  )
  } else {
-   return <h3>Loading...</h3>
+  return <h3>Loading...</h3>
  }
 }
 
