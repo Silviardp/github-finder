9c1d064c03fe30498bbbeb3e8a3725187950ead8
Silvia Rocha <silviadprocha@gmail.com>
2022-02-02 21:29:20 +0100

    added UserSearch component

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -4,7 +4,7 @@ import GithubContext from '../../context/github/GithubContext'
 function UserSearch() {
  const [text, setText] = useState('')
 
- const {users} = useContext(GithubContext)
+ const {users, searchUsers} = useContext(GithubContext)
 
  const handleChange = (e) => setText(e.target.value)
 
@@ -14,13 +14,13 @@ function UserSearch() {
   if(text === '') {
    alert('Please enter something')
   } else {
-   // @todo: search users
-
+   searchUsers(text)
    setText('')
   }
  }
 
-  return <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
+  return (
+  <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
    <div>
    <form onSubmit={handleSubmit}> 
     <div className="form-control">
@@ -47,8 +47,8 @@ function UserSearch() {
      </button>
    </div>)
     }
-
-  </div>;
+  </div>
+  )
 }
 
 export default UserSearch;
